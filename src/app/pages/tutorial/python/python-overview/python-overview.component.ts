06fe390389d119a73601f58203db42a813d71b97
sukantahui <huisukanta77@gmail.com>
2021-05-07 15:25:21 +0530

    scrolling working

--- src/app/pages/tutorial/python/python-overview/python-overview.component.ts
+++ src/app/pages/tutorial/python/python-overview/python-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-python-overview',
@@ -6,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./python-overview.component.scss']
 })
 export class PythonOverviewComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {
+    this.route.fragment.subscribe(f => {
+      const element = document.querySelector('#' + f);
+      if (element) {
+        element.scrollIntoView(true);
+      }
+    });
+  }
 
-  constructor() { }
+  jumpToId( fragment ) {
 
+    // Use the browser to navigate
+    window.location.hash = fragment;
+
+    // But also scroll when routing / deep-linking to dynamic page
+    // or re-clicking same anchor
+    if (fragment) {
+      const element = document.querySelector('#' + fragment);
+      if (element) { element.scrollIntoView(); }
+    }
+  }
   ngOnInit(): void {
   }
 
